c938b1ff90254bf3d0d839ff9c182b52c9e2d6c0
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-13 20:03:40 +0300

    feature: add description for each tag in tag util

--- src/utils/tag.ts
+++ src/utils/tag.ts
@@ -20,3 +20,12 @@ export const iconByTag: TagMapping = {
   negative: '😢',
   neutral: '😶',
 }
+
+export const descByTag: TagMapping = {
+  positive:
+    'A positive sentiment tag indicates that the sentiment of a piece of text is generally positive. This could mean that the text expresses a favorable opinion, conveys happiness or excitement, or generally has a positive tone',
+  negative:
+    'A negative sentiment tag indicates that the sentiment of a piece of text is generally negative. This could mean that the text expresses a critical opinion, conveys sadness or disappointment, or generally has a negative tone',
+  neutral:
+    ' A neutral sentiment tag indicates that the sentiment of a piece of text is generally neutral or without a clear positive or negative sentiment. This could mean that the text is factual, informative, or does not express a strong opinion or emotion',
+}
