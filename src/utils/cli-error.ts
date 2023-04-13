89ccabbe44e99a3244cac5adc12b41a06d26ef19
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-13 16:21:12 +0300

    feature: setup cli error util

---
+++ src/utils/cli-error.ts
@@ -0,0 +1,21 @@
+import color from 'picocolors'
+import { AUTHOR, PROJECT_NAME, VERSION } from './constants'
+
+export class CliError extends Error {}
+
+const indent = ' '.repeat(4)
+
+export const handleCliError = (error: any) => {
+  if (error instanceof Error && !(error instanceof CliError)) {
+    if (error.stack) {
+      console.error(color.dim(error.stack.split('\n').slice(1).join('\n')))
+    }
+    console.error(`\n${indent}${color.dim(`${PROJECT_NAME} v${VERSION}`)}`)
+    console.error(
+      `\n${indent}Please open a Bug report with the information above:`
+    )
+    console.error(
+      `${indent}https://github.com/${AUTHOR}/${PROJECT_NAME}/issues/new`
+    )
+  }
+}
